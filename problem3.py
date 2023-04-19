c08723f27d591449cfb9064ee48b567ea987fccb
Andrew Wasielewski <awwasielewski@gmail.com>
2023-04-19 12:55:50 -0700

    DID NUMBER THREE

--- problem3.py
+++ problem3.py
@@ -7,7 +7,8 @@ What is the largest prime factor of the number 600851475143 ?
 """
 
 def is_prime(n):
-    n = int(n)
+    if not isinstance(n, int):
+        return False 
     for i in range(2, (n // 2) + 1):
         if (n / i).is_integer():
             return False
@@ -21,21 +22,21 @@ def yield_primes():
         while not is_prime(n):
             n += 1
             
-    
-   
-
+l = []
 def f(n):
-    l = []
     i = 3
-    while len(l) < 3:
-        if is_prime(n / i):
-            l.append(i)
-            l.append(int(n / i))
-        else:
-            i = next(prime)
+    t = yield_primes()
+
+    if 1 in l:
+        return max(set(l))
+
+    while n % i != 0:
+        i = next(t)    
+
+    l.append(i)
+    if is_prime(int(n/i)):
+        l.append(int(n / i))
+    return f(int(n/i))
          
 
-    return l
-
-
-print(f(13195))
+print(f(600851475143))
