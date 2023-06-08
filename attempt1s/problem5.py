a1fd612f825478a9f454cc167c43e3a2b84f4a68
Andrew Wasielewski <awwasielewski@gmail.com>
2023-06-08 00:27:52 -0400

    problem 5 attempt 1:

--- attempt1s/problem5.py
+++ attempt1s/problem5.py
@@ -1,4 +1,4 @@
- Smallest multiple
+#Smallest multiple
 
 """
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
@@ -6,15 +6,34 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
 
+# There is def a recursive solution for this 
+# Maybe in my next attempt I'll try it 
+
+# ^^ Actually probably not because recursion can't go as deep as it would need to for
+# this 
+
 def f(n):
     x = n + 1
-    for i in range(2, n + 1):
-        if x % i != 0:
-           f(n + 1) 
-        else:
-            return x
-         
+    while not (x % 10 == 0 and x % 9 == 0 and x % 8 == 0 and x % 7 == 0 and x % 6 == 0):
+        x += 1
 
-print(f(10))
+    return x
 
+
+
+def g(n):
+    x = n + 1
+
+    while not (x % 20 == 0 and x % 19 == 0 and x % 18 == 0 and x % 17 == 0 and x % 16 ==
+0 and x % 15 == 0 and x % 14 == 0 and x % 13 == 0 and x % 12 == 0 and x % 11 == 0):
+        x += 1
+
+    return x
+    
+print(g(20))
+
+# This would be a lot cleaner if we could figure out how to just check divisibility by a range 
+
+# Is there some fundamental math that I'm missing that would make the computation
+# quicker??
 
